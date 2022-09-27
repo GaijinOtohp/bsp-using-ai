ec3ca9f09640d29b7cc147d46e8260cc4d0a4b4f
SMURF <m-mohamed-a@hotmail.com>
2022-09-27 12:21:59 +0100

    Create State class as structure

---
+++ Structures.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biological_Signal_Processing_Using_AI
+{
+    internal class Structures
+    {
+        public class State
+        {
+            public string Name;
+            public int _index;
+            public double _value;
+            public double _meanFromLastState;
+            public double _tangentFromLastState;
+            public double _meanTangentFromLastState;
+            public double _deviantionAngle;
+        }
+    }
+}
