d23ad29b85c5142be533c921b4b5ae8d98a926c2
SMURF <m-mohamed-a@hotmail.com>
2023-02-12 01:44:36 +0100

    Fix the size of dataset flow layout panel where the items fit together with the scroll bar

--- BSP Using AI/AITools/DatasetExplorer/DatasetExplorerForm.Designer.cs
+++ BSP Using AI/AITools/DatasetExplorer/DatasetExplorerForm.Designer.cs
@@ -46,20 +46,19 @@ namespace BSP_Using_AI.AITools.DatasetExplorer
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.signalsFlowLayoutPanel.AutoScroll = true;
-            this.signalsFlowLayoutPanel.Location = new System.Drawing.Point(3, 122);
-            this.signalsFlowLayoutPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.signalsFlowLayoutPanel.Location = new System.Drawing.Point(2, 99);
             this.signalsFlowLayoutPanel.Name = "signalsFlowLayoutPanel";
-            this.signalsFlowLayoutPanel.Size = new System.Drawing.Size(910, 554);
+            this.signalsFlowLayoutPanel.Size = new System.Drawing.Size(700, 450);
             this.signalsFlowLayoutPanel.TabIndex = 1;
             this.signalsFlowLayoutPanel.SizeChanged += new System.EventHandler(this.signalsFlowLayoutPanel_SizeChanged);
             // 
             // unselectAllButton
             // 
             this.unselectAllButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.unselectAllButton.Location = new System.Drawing.Point(758, 14);
-            this.unselectAllButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.unselectAllButton.Location = new System.Drawing.Point(586, 11);
+            this.unselectAllButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.unselectAllButton.Name = "unselectAllButton";
-            this.unselectAllButton.Size = new System.Drawing.Size(143, 27);
+            this.unselectAllButton.Size = new System.Drawing.Size(107, 22);
             this.unselectAllButton.TabIndex = 27;
             this.unselectAllButton.Text = "Unselect all";
             this.unselectAllButton.UseVisualStyleBackColor = true;
@@ -68,10 +67,10 @@ namespace BSP_Using_AI.AITools.DatasetExplorer
             // deleteSelectionButton
             // 
             this.deleteSelectionButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.deleteSelectionButton.Location = new System.Drawing.Point(613, 14);
-            this.deleteSelectionButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.deleteSelectionButton.Location = new System.Drawing.Point(478, 11);
+            this.deleteSelectionButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.deleteSelectionButton.Name = "deleteSelectionButton";
-            this.deleteSelectionButton.Size = new System.Drawing.Size(140, 27);
+            this.deleteSelectionButton.Size = new System.Drawing.Size(105, 22);
             this.deleteSelectionButton.TabIndex = 28;
             this.deleteSelectionButton.Text = "Delete selection";
             this.deleteSelectionButton.UseVisualStyleBackColor = true;
@@ -80,10 +79,10 @@ namespace BSP_Using_AI.AITools.DatasetExplorer
             // samplingRateLabel
             // 
             this.samplingRateLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.samplingRateLabel.Location = new System.Drawing.Point(396, 86);
-            this.samplingRateLabel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.samplingRateLabel.Location = new System.Drawing.Point(289, 70);
+            this.samplingRateLabel.Margin = new System.Windows.Forms.Padding(3, 3, 3, 3);
             this.samplingRateLabel.Name = "samplingRateLabel";
-            this.samplingRateLabel.Size = new System.Drawing.Size(116, 28);
+            this.samplingRateLabel.Size = new System.Drawing.Size(87, 23);
             this.samplingRateLabel.TabIndex = 31;
             this.samplingRateLabel.Text = "Sampling rate";
             this.samplingRateLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -91,10 +90,10 @@ namespace BSP_Using_AI.AITools.DatasetExplorer
             // startingIndexLabel
             // 
             this.startingIndexLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.startingIndexLabel.Location = new System.Drawing.Point(244, 86);
-            this.startingIndexLabel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.startingIndexLabel.Location = new System.Drawing.Point(175, 70);
+            this.startingIndexLabel.Margin = new System.Windows.Forms.Padding(3, 3, 3, 3);
             this.startingIndexLabel.Name = "startingIndexLabel";
-            this.startingIndexLabel.Size = new System.Drawing.Size(144, 28);
+            this.startingIndexLabel.Size = new System.Drawing.Size(108, 23);
             this.startingIndexLabel.TabIndex = 30;
             this.startingIndexLabel.Text = "Starting index (sec)";
             this.startingIndexLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -102,22 +101,22 @@ namespace BSP_Using_AI.AITools.DatasetExplorer
             // signalNameLabel
             // 
             this.signalNameLabel.AutoSize = true;
-            this.signalNameLabel.Location = new System.Drawing.Point(8, 86);
-            this.signalNameLabel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.signalNameLabel.MaximumSize = new System.Drawing.Size(217, 0);
-            this.signalNameLabel.MinimumSize = new System.Drawing.Size(217, 28);
+            this.signalNameLabel.Location = new System.Drawing.Point(6, 70);
+            this.signalNameLabel.Margin = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.signalNameLabel.MaximumSize = new System.Drawing.Size(163, 0);
+            this.signalNameLabel.MinimumSize = new System.Drawing.Size(163, 23);
             this.signalNameLabel.Name = "signalNameLabel";
-            this.signalNameLabel.Size = new System.Drawing.Size(217, 28);
+            this.signalNameLabel.Size = new System.Drawing.Size(163, 23);
             this.signalNameLabel.TabIndex = 29;
             this.signalNameLabel.Text = "Signal name";
             this.signalNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // fitSelectionButton
             // 
-            this.fitSelectionButton.Location = new System.Drawing.Point(19, 14);
-            this.fitSelectionButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.fitSelectionButton.Location = new System.Drawing.Point(14, 11);
+            this.fitSelectionButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.fitSelectionButton.Name = "fitSelectionButton";
-            this.fitSelectionButton.Size = new System.Drawing.Size(140, 27);
+            this.fitSelectionButton.Size = new System.Drawing.Size(105, 22);
             this.fitSelectionButton.TabIndex = 32;
             this.fitSelectionButton.Text = "Fit selection";
             this.fitSelectionButton.UseVisualStyleBackColor = true;
@@ -127,10 +126,9 @@ namespace BSP_Using_AI.AITools.DatasetExplorer
             // instrucitonLabel
             // 
             this.instrucitonLabel.AutoSize = true;
-            this.instrucitonLabel.Location = new System.Drawing.Point(16, 43);
-            this.instrucitonLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.instrucitonLabel.Location = new System.Drawing.Point(12, 35);
             this.instrucitonLabel.Name = "instrucitonLabel";
-            this.instrucitonLabel.Size = new System.Drawing.Size(196, 16);
+            this.instrucitonLabel.Size = new System.Drawing.Size(158, 13);
             this.instrucitonLabel.TabIndex = 33;
             this.instrucitonLabel.Text = "(Click Shift for multiple selection)";
             this.instrucitonLabel.Visible = false;
@@ -138,19 +136,19 @@ namespace BSP_Using_AI.AITools.DatasetExplorer
             // quantizationStepLabel
             // 
             this.quantizationStepLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.quantizationStepLabel.Location = new System.Drawing.Point(520, 86);
-            this.quantizationStepLabel.Margin = new System.Windows.Forms.Padding(4);
+            this.quantizationStepLabel.Location = new System.Drawing.Point(382, 70);
+            this.quantizationStepLabel.Margin = new System.Windows.Forms.Padding(3, 3, 3, 3);
             this.quantizationStepLabel.Name = "quantizationStepLabel";
-            this.quantizationStepLabel.Size = new System.Drawing.Size(136, 28);
+            this.quantizationStepLabel.Size = new System.Drawing.Size(102, 23);
             this.quantizationStepLabel.TabIndex = 34;
             this.quantizationStepLabel.Text = "Quantization step";
             this.quantizationStepLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // DatasetExplorerForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(916, 678);
+            this.ClientSize = new System.Drawing.Size(705, 551);
             this.Controls.Add(this.quantizationStepLabel);
             this.Controls.Add(this.instrucitonLabel);
             this.Controls.Add(this.fitSelectionButton);
@@ -161,7 +159,6 @@ namespace BSP_Using_AI.AITools.DatasetExplorer
             this.Controls.Add(this.unselectAllButton);
             this.Controls.Add(this.signalsFlowLayoutPanel);
             this.KeyPreview = true;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "DatasetExplorerForm";
             this.Text = "DatasetExplorerForm";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.DatasetExplorerForm_KeyDown);
